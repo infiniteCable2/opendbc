77dfb2b1fa26fcad01a0398c94bb379245470d81
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-01-01 17:14:36 +0100

    Update interface.py VW MEB use curvature controller

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -38,7 +38,7 @@ class CarInterface(CarInterfaceBase):
       ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.volkswagenMeb)]
       ret.enableBsm = 0x24C in fingerprint[0]  # MEB_Side_Assist_01
       ret.transmissionType = TransmissionType.direct
-      ret.steerControlType = structs.CarParams.SteerControlType.angle
+      ret.steerControlType = structs.CarParams.SteerControlType.curvatureDEPRECATED
 
       if any(msg in fingerprint[1] for msg in (0x520, 0x86, 0xFD, 0x13D)):  # Airbag_02, LWI_01, ESP_21, MEB_EPS_01
         ret.networkLocation = NetworkLocation.gateway
