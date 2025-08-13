01ceea8eba1bd0c8d220a38fc31ade835e715e08
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-08-13 18:29:40 +0200

    Update volkswagen_common.h

--- opendbc/safety/modes/volkswagen_common.h
+++ opendbc/safety/modes/volkswagen_common.h
@@ -2,13 +2,9 @@
 
 extern const uint16_t FLAG_VOLKSWAGEN_LONG_CONTROL;
 extern const uint16_t FLAG_VOLKSWAGEN_ALT_CRC_VARIANT_1;
-extern const uint16_t FLAG_VOLKSWAGEN_ALT_CANCEL_BUTTON;
-extern const uint16_t FLAG_VOLKSWAGEN_NO_CRUISE_STATE;
 
 const uint16_t FLAG_VOLKSWAGEN_LONG_CONTROL = 1;
 const uint16_t FLAG_VOLKSWAGEN_ALT_CRC_VARIANT_1 = 2;
-const uint16_t FLAG_VOLKSWAGEN_ALT_CANCEL_BUTTON = 4;
-const uint16_t FLAG_VOLKSWAGEN_NO_CRUISE_STATE = 8;
 
 static uint8_t volkswagen_crc8_lut_8h2f[256]; // Static lookup table for CRC8 poly 0x2F, aka 8H2F/AUTOSAR
 
@@ -22,11 +18,6 @@ extern bool volkswagen_resume_button_prev;
 bool volkswagen_resume_button_prev = false;
 
 extern bool volkswagen_alt_crc_variant_1;
-extern bool volkswagen_alt_cancel_button;
-extern bool volkswagen_no_cruise_state;
-
-bool volkswagen_alt_crc_variant_1 = false;
-bool volkswagen_alt_cancel_button = false;
 bool volkswagen_no_cruise_state = false;
 
 
