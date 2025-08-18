cf7eb262ac00e63f206416fe77d71fa15a3da268
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-08-18 21:15:06 +0200

    test

--- opendbc/car/volkswagen/carstate.py
+++ opendbc/car/volkswagen/carstate.py
@@ -308,7 +308,7 @@ class CarState(CarStateBase):
     # Infostufe: BSM LED on, Warnung: BSM LED flashing
     if self.CP.enableBsm:
       # TODO get side assignment
-      bsm_bus = pt_cp if ["MEB_Side_Assist_01"] in pt_cp else ext_cp
+      bsm_bus = pt_cp if "MEB_Side_Assist_01" in pt_cp else ext_cp
       ret.leftBlindspot  = bool(bsm_bus.vl["MEB_Side_Assist_01"]["Blind_Spot_Info_Driver"]) or bool(bsm_bus.vl["MEB_Side_Assist_01"]["Blind_Spot_Warn_Driver"])
       ret.rightBlindspot = bool(bsm_bus.vl["MEB_Side_Assist_01"]["Blind_Spot_Info_Passenger"]) or bool(bsm_bus.vl["MEB_Side_Assist_01"]["Blind_Spot_Warn_Passenger"])
 
