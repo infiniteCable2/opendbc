37236fdcfbc853b1c6d7440094fa28ff78f5635e
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-08-22 22:06:14 +0200

    a little bit of refactor and comment, final PoC for now

--- opendbc/sunnypilot/car/volkswagen/mads.py
+++ opendbc/sunnypilot/car/volkswagen/mads.py
@@ -5,31 +5,41 @@ This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
 
-from enum import StrEnum
-
-from opendbc.car import Bus,structs
+from opendbc.car import structs
 
 from opendbc.sunnypilot.mads_base import MadsCarStateBase
 from opendbc.can.parser import CANParser
+from opendbc.car.volkswagen.values import GearShifter
 
 ButtonType = structs.CarState.ButtonEvent.Type
+
+TOLERANCE_MAX     = 100 # 1 second
+TEMP_CRUISE_FAULT = 6
 
 
 class MadsCarState(MadsCarStateBase):
   def __init__(self, CP: structs.CarParams, CP_SP: structs.CarParamsSP):
     super().__init__(CP, CP_SP)
+    self.tolerance_counter = TOLERANCE_MAX
 
   def update_mads(self, ret: structs.CarState, can_parser_pt: CANParser) -> None:
     self.prev_lkas_button = self.lkas_button
 
-    # block mads from detecting a cruise state transition in parked mode while cruise is temporary not available
-    parking_brake_not_fully_open = can_parser_pt.vl["ESC_50"]["EPB_Status"] in (1, 3, 4)
-    temp_cruise_fault = can_parser_pt.vl["Motor_51"]["TSK_Status"] == 6
-    if temp_cruise_fault and parking_brake_not_fully_open:
+    # safely block MADS from detecting a cruise state transition from parked mode while cruise is temporary not available
+    # (blocking user unintended MADS enablement via main cruise state mechanism)
+    temp_cruise_fault = can_parser_pt.vl["Motor_51"]["TSK_Status"] == TEMP_CRUISE_FAULT
+    drive_mode        = ret.gearShifter == GearShifter.drive
+    
+    if temp_cruise_fault and ret.parkingBrake and not drive_mode:
       ret.cruiseState.available = True
+      self.tolerance_counter    = 0
+      
+    elif self.tolerance_counter < TOLERANCE_MAX: # grant a little bit of time for the car doing its state transition
+      ret.cruiseState.available = True
+      self.tolerance_counter    = min(self.tolerance_counter + 1, TOLERANCE_MAX)
     
     # some newer gen MEB cars do not have a main cruise button and a native cancel button is present
-    # cruise state can not be fully toggled off for these cars!    
+    # WARNING: cruise state can not be fully toggled off for these cars!    
     user_disable = False
          
     for b in ret.buttonEvents:
@@ -37,5 +47,5 @@ class MadsCarState(MadsCarStateBase):
         user_disable = True
         break
     
-    steering_enabled = can_parser_pt.vl["QFK_01"]["LatCon_HCA_Status"] == "ACTIVE" # presume mads is actively steering
+    steering_enabled = can_parser_pt.vl["QFK_01"]["LatCon_HCA_Status"] == "ACTIVE" # assume mads is actively steering
     self.lkas_button = steering_enabled and user_disable
