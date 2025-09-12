63f00ae6e10c464aba7f6b80dd784d0b593dfe21
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-09-12 18:45:31 +0200

    Update interface.py get this out for now to test our new jerk control

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -114,10 +114,10 @@ class CarInterface(CarInterfaceBase):
     if ret.flags & VolkswagenFlags.MEB:
       ret.longitudinalActuatorDelay = 0.4
       ret.radarDelay = 0.4
-      ret.longitudinalTuning.kpBP = [0., 20.]
-      ret.longitudinalTuning.kiBP = [0., 10.]
-      ret.longitudinalTuning.kpV = [0.6, 0.]
-      ret.longitudinalTuning.kiV = [0.1, 0.]
+      #ret.longitudinalTuning.kpBP = [0., 20.]
+      #ret.longitudinalTuning.kiBP = [0., 10.]
+      #ret.longitudinalTuning.kpV = [0.6, 0.]
+      #ret.longitudinalTuning.kiV = [0.1, 0.]
 
     ret.alphaLongitudinalAvailable = ret.networkLocation == NetworkLocation.gateway or docs
     if alpha_long:
