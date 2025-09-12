015b14aea7de59e97b545851eae25677fa307036
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-09-12 20:18:12 +0200

    Update interface.py higher delay long actuator

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -112,7 +112,7 @@ class CarInterface(CarInterfaceBase):
     # Global longitudinal tuning defaults, can be overridden per-vehicle
 
     if ret.flags & VolkswagenFlags.MEB:
-      ret.longitudinalActuatorDelay = 0.4
+      ret.longitudinalActuatorDelay = 0.5
       ret.radarDelay = 0.4
       ret.longitudinalTuning.kiBP = [0., 5., 35.]
       ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5]
