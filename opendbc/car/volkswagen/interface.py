450bd3d0db4653e576214c2d94db11028384c313
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-09-16 18:56:06 +0200

    Update interface.py

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -115,7 +115,7 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalActuatorDelay = 0.5
       ret.radarDelay = 0.2
       ret.longitudinalTuning.kiBP = [0., 5., 35.]
-      ret.longitudinalTuning.kiV = [1.4, 0.7, 0.2]
+      ret.longitudinalTuning.kiV = [1., 0.5, 0.2]
 
     ret.alphaLongitudinalAvailable = ret.networkLocation == NetworkLocation.gateway or docs
     if alpha_long:
