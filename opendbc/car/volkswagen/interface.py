8edbd4024cdf7e512751285be811d8a4b8989517
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-09-26 18:07:42 +0200

    Revert "VW Long Optimization (#12)"
    This reverts commit 6494f74486c2b0affcf6d2ed36d4976fbd63c813.

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -112,12 +112,12 @@ class CarInterface(CarInterfaceBase):
     # Global longitudinal tuning defaults, can be overridden per-vehicle
 
     if ret.flags & VolkswagenFlags.MEB:
-      ret.longitudinalActuatorDelay = 0.5
-      ret.radarDelay = 0.8
-      ret.longitudinalTuning.kpBP = [0., 5.]
-      ret.longitudinalTuning.kiBP = [0., 30.]
-      ret.longitudinalTuning.kpV = [0.4, 0.] # (with usage of starting state otherwise starting jerk)
-      ret.longitudinalTuning.kiV = [0.6, 0.]
+      ret.longitudinalActuatorDelay = 0.4
+      ret.radarDelay = 0.4
+      ret.longitudinalTuning.kpBP = [0., 20.]
+      ret.longitudinalTuning.kiBP = [0., 10.]
+      ret.longitudinalTuning.kpV = [0.6, 0.]
+      ret.longitudinalTuning.kiV = [0.1, 0.]
 
     ret.alphaLongitudinalAvailable = ret.networkLocation == NetworkLocation.gateway or docs
     if alpha_long:
@@ -131,11 +131,13 @@ class CarInterface(CarInterfaceBase):
     ret.autoResumeSng = ret.minEnableSpeed == -1
 
     if ret.flags & VolkswagenFlags.MEB:
-      ret.startingState = True # OP long starting state is used: for very slow start the car can go into error (EPB car shutting down bug)
-      ret.startAccel = 0.8
+      ret.startingState = True # OP long starting state is used
+      ret.startAccel = 0.85 # ~0.85 m/s^2 for brake release
       ret.vEgoStarting = 0.5 # minimum ~0.5 m/s acc starting state is neccessary to not fault the car
       ret.vEgoStopping = 0.1
-      ret.stopAccel = -0.55 # different stopping accels seen, good working value
+      ret.stopAccel = -0.55 # try a good balance, maybe new gen car faults only for very low -1.1 stop accel
+      #ret.stopAccel = -0.2 # stock stopping accel for newer gen car, stopped accel is volkswagen neutral value
+      #ret.stopAccel = -1.1 # stock stopped accel
     else:
       ret.vEgoStarting = 0.1
       ret.vEgoStopping = 0.5
