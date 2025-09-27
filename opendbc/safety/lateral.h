70b8cb1f32a020b60b159444c7d218732e221f5d
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-09-27 22:36:52 +0200

    Update lateral.h

--- opendbc/safety/lateral.h
+++ opendbc/safety/lateral.h
@@ -408,7 +408,7 @@ bool steer_angle_cmd_checks_vm(int desired_angle, bool steer_control_enabled, co
 bool steer_power_cmd_checks(int desired_steer_power, bool steer_control_enabled, const CurvatureSteeringLimits limits) {
   bool violation = false;
 
-  violation |= max_limit_check(desired_steer_power, limits.max_power, -limits.max_power);
+  violation |= max_limit_check(desired_steer_power, limits.max_power, 0);
   violation |= desired_steer_power > 0 && !steer_control_enabled;
   violation |= !is_lat_active() && steer_control_enabled && desired_steer_power != 0 && desired_steer_power >= desired_steer_power_last;
   violation |= !is_lat_active() && !steer_control_enabled && desired_steer_power != 0;
