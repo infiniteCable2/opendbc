f4c242d1d489df9e31de817a7ad46d45bbefa37c
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-10-01 21:51:04 +0200

    Update test_volkswagen_meb.py

--- opendbc/safety/tests/test_volkswagen_meb.py
+++ opendbc/safety/tests/test_volkswagen_meb.py
@@ -134,7 +134,7 @@ class TestVolkswagenMebStockSafety(TestVolkswagenMebSafetyBase):
   TX_MSGS = [[MSG_HCA_03, 0], [MSG_LDW_02, 0], [MSG_GRA_ACC_01, 0],
              [MSG_EA_01, 0], [MSG_EA_02, 0], [MSG_KLR_01, 0], [MSG_KLR_01, 2]]
   FWD_BLACKLISTED_ADDRS = {0: [MSG_LH_EPS_03], 2: [MSG_HCA_03, MSG_LDW_02]}
-  RELAY_MALFUNCTION_ADDRS = {0: (MSG_HCA_03, MSG_LDW_02)}
+  RELAY_MALFUNCTION_ADDRS = {0: (MSG_HCA_03, MSG_LDW_02, MSG_EA_02)}
 
   def setUp(self):
     self.packer = CANPackerPanda("vw_meb")
@@ -155,7 +155,7 @@ class TestVolkswagenMebStockSafety(TestVolkswagenMebSafetyBase):
 class TestVolkswagenMebCurvatureSafety(TestVolkswagenMebSafetyBase, common.CurvatureSteeringSafetyTest):
   TX_MSGS = [[MSG_HCA_03, 0]]
   FWD_BLACKLISTED_ADDRS = {0: [MSG_LH_EPS_03], 2: [MSG_HCA_03, MSG_LDW_02]}
-  RELAY_MALFUNCTION_ADDRS = {0: (MSG_HCA_03, MSG_LDW_02)}
+  RELAY_MALFUNCTION_ADDRS = {0: (MSG_HCA_03, MSG_LDW_02, MSG_EA_02)}
 
   def setUp(self):
     self.packer = CANPackerPanda("vw_meb")
@@ -170,7 +170,7 @@ class TestVolkswagenMebLongSafety(TestVolkswagenMebSafetyBase):
              [MSG_KLR_01, 0], [MSG_KLR_01, 2]]
   FWD_BLACKLISTED_ADDRS = {0: [MSG_LH_EPS_03],
                            2: [MSG_HCA_03, MSG_LDW_02, MSG_ACC_18, MSG_MEB_ACC_01]}
-  RELAY_MALFUNCTION_ADDRS = {0: (MSG_HCA_03, MSG_LDW_02, MSG_ACC_18, MSG_MEB_ACC_01)}
+  RELAY_MALFUNCTION_ADDRS = {0: (MSG_HCA_03, MSG_LDW_02, MSG_ACC_18, MSG_MEB_ACC_01, MSG_EA_02)}
 
   ALLOW_OVERRIDE = True
   ACCEL_OVERRIDE = 0
