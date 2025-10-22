23a1fd39ac01b64f95239c6ef5bba91e7ca93c47
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-10-22 17:38:40 +0200

    Update icbm.py timing by frame

--- opendbc/sunnypilot/car/volkswagen/icbm.py
+++ opendbc/sunnypilot/car/volkswagen/icbm.py
@@ -5,7 +5,7 @@ This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
 
-from opendbc.car import structs
+from opendbc.car import DT_CTRL, structs
 from opendbc.car.can_definitions import CanData
 from opendbc.car.volkswagen import pqcan, mqbcan, mebcan
 from opendbc.car.volkswagen.values import VolkswagenFlags
@@ -19,10 +19,11 @@ class IntelligentCruiseButtonManagementInterface(IntelligentCruiseButtonManageme
     super().__init__(CP, CP_SP)
     self.CCS = pqcan if CP.flags & VolkswagenFlags.PQ else (mebcan if CP.flags & VolkswagenFlags.MEB else mqbcan)
 
-  def update(self, CC_SP, CS, packer, bus_send) -> list[CanData]:
+  def update(self, CC_SP, CS, packer, frame, CAN) -> list[CanData]:
     can_sends = []
     self.CC_SP = CC_SP
     self.ICBM = CC_SP.intelligentCruiseButtonManagement
+    self.frame = frame
 
     up = self.ICBM.sendButton == SendButtonState.increase
     down = self.ICBM.sendButton == SendButtonState.decrease
@@ -30,6 +31,8 @@ class IntelligentCruiseButtonManagementInterface(IntelligentCruiseButtonManageme
     # set and resume buttons are used to achieve +1 and -1 button presses
     # make sure cruise state is already enabled to not enable car cruise user unintended
     if CS.out.cruiseState.enabled and (up or down):
-      can_sends.append(self.CCS.create_acc_buttons_control(packer, bus_send, CS.gra_stock_values, up=up, down=down))
-
+      if (self.frame - self.last_button_frame) * DT_CTRL > 0.2:
+        can_sends.append(self.CCS.create_acc_buttons_control(packer, CAN, CS.gra_stock_values, up=up, down=down))
+        self.last_button_frame = self.frame
+    
     return can_sends
