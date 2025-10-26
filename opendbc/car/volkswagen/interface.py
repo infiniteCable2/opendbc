be73132e6de2c51311601a120b9cd6d9788af239
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-10-26 14:06:03 +0100

    Volkswagen MQBevo Support (#19)
    * initial plausible adaption
    
    * lane assist hud signal too
    
    * Update interface.py
    
    * like this for now
    
    * more
    
    * Update carstate.py
    
    * Update carstate.py
    
    * Update carstate.py
    
    * Update carstate.py
    
    * Update carstate.py
    
    * Update vw_mqbevo.dbc add/update neccessary signals
    
    * Update vw_mqbevo.dbc
    
    * Update fingerprints.py
    
    * Update carstate.py
    
    * Update interface.py use this for vego cluster
    
    * Update vw_mqbevo.dbc format error
    
    * Update vw_mqbevo.dbc
    
    * Update values.py
    
    * Update carcontroller.py
    
    * Update carstate.py
    
    * Update interface.py
    
    * Update radar_interface.py allow radar for mqbevo
    
    * Update radar_interface.py
    
    * Update vw_mqbevo.dbc add distance signal
    
    * Update vw_mqbevo.dbc fix accel pedal scaling
    
    * Update vw_meb.dbc
    
    * Update vw_meb_2024.dbc
    
    * Update volkswagen_meb.h use motor_51 in general, motor_54 does have another offset for meb and mqb evo
    
    * Update carstate.py use Motor_51 for Accelerator detection
    
    * Update vw_meb_2024.dbc
    
    * Update vw_meb.dbc
    
    * Update vw_mqbevo.dbc
    
    * Revert "Update vw_mqbevo.dbc"
    
    This reverts commit c6c4fd9bc3b3ab40fbff32636bdc5c8683394fd1.
    
    * Revert "Update vw_meb.dbc"
    
    This reverts commit 2e5d39246066a90de2994e0ad5b5295be17f65ad.
    
    * Revert "Update vw_meb_2024.dbc"
    
    This reverts commit 171422f612c01e9c4bdcc3109e9e810f02f543fc.
    
    * Revert "Update carstate.py use Motor_51 for Accelerator detection"
    
    This reverts commit fe4b784408ee39a54b8fe3964e5080cba3243d3e.
    
    * Revert "Update volkswagen_meb.h use motor_51 in general, motor_54 does have another offset for meb and mqb evo"
    
    This reverts commit 9ccbe999e0df7e93924c253bf91e812291428e70.
    
    * Update carstate.py offset for mqb evo gas motor_54
    
    * Update volkswagen_common.h
    
    * Update volkswagen_meb.h
    
    * Update volkswagen_common.h
    
    * Update values.py
    
    * Update interface.py
    
    * Update carstate.py fix
    
    * Update carstate.py comment
    
    * Update volkswagen_meb.h fix
    
    * Update carcontroller.py fix
    
    * Update carstate.py fix
    
    * Update interface.py fix
    
    * Update values.py fix
    
    * Update speed_limit_manager.py
    
    * Update radar_interface.py
    
    * Update speed_limit_manager.py Mensch
    
    * Update carstate.py fix
    
    * Update volkswagen_meb.h
    
    * Update vw_meb.dbc motor_54 accel offset to 0
    
    * Update vw_meb_2024.dbc motor_54 accel offset to 0
    
    * Update carstate.py offset now in DBCs
    
    * Update vw_mqbevo.dbc add ea signals
    
    * Update carcontroller.py fix
    
    * Update volkswagen_meb.h wrong sign gas
    
    * Update fingerprints.py add fp seat leon mk4
    
    * Update values.py add another mkqevo car
    
    * adapt
    
    * Update interface.py
    
    * Update interface.py
    
    * Update carstate.py workaround for signals whose bus is dynamically detected
    
    * Update carstate.py
    
    * Update fingerprints.py add golf 8 2024
    
    * Update values.py
    
    * Update fingerprints.py
    
    * Update routes.py
    
    * Update override.toml
    
    * Update car_list.json
    
    * Update vw_mqbevo.dbc fix distance signal scalings
    
    * Update volkswagen_meb.h
    
    * Update car_list.json generic car list entry for mqbevo
    
    * Update values.py fix
    
    * Update volkswagen_meb.h fix gas offset MEB
    
    ---------
    
    Co-authored-by: infiniteCable <75014343+infiniteCable@users.noreply.github.com>

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -39,11 +39,18 @@ class CarInterface(CarInterfaceBase):
       # Panda ALLOW_DEBUG firmware required.
       #ret.dashcamOnly = True
 
-    elif ret.flags & VolkswagenFlags.MEB:
+    elif ret.flags & (VolkswagenFlags.MEB | VolkswagenFlags.MQB_EVO):
       # Set global MEB parameters
-      safety_configs = [get_safety_config(structs.CarParams.SafetyModel.volkswagenMeb)]
+      if ret.flags & VolkswagenFlags.MEB:
+        safety_configs = [get_safety_config(structs.CarParams.SafetyModel.volkswagenMeb)]
+      elif ret.flags & VolkswagenFlags.MQB_EVO:
+        safety_configs = [get_safety_config(structs.CarParams.SafetyModel.volkswagenMqbEvo)]
+        
       if ret.flags & VolkswagenFlags.MEB_GEN2:
         safety_configs[0].safetyParam |= VolkswagenSafetyFlags.ALT_CRC_VARIANT_1.value
+
+      if ret.flags & VolkswagenFlags.MQB_EVO:
+        safety_configs[0].safetyParam |= VolkswagenSafetyFlags.NO_GAS_OFFSET.value
       
       ret.enableBsm = 0x24C in fingerprint[0]  # MEB_Side_Assist_01
       ret.transmissionType = TransmissionType.direct
@@ -59,7 +66,7 @@ class CarInterface(CarInterfaceBase):
       if ret.networkLocation == NetworkLocation.gateway:
         ret.radarUnavailable = False
         
-      if 0x6B8 in fingerprint[0]:  # Kombi_03
+      if 0x30B in fingerprint[0]:  # Kombi_01
         ret.flags |= VolkswagenFlags.KOMBI_PRESENT.value
 
       if 0x25D in fingerprint[0]:  # KLR_01
@@ -102,7 +109,7 @@ class CarInterface(CarInterfaceBase):
     if ret.flags & VolkswagenFlags.PQ:
       ret.steerActuatorDelay = 0.3
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
-    elif ret.flags & VolkswagenFlags.MEB:
+    elif ret.flags & (VolkswagenFlags.MEB | VolkswagenFlags.MQB_EVO):
       ret.steerActuatorDelay = 0.3
     else:
       ret.steerActuatorDelay = 0.1
@@ -133,7 +140,7 @@ class CarInterface(CarInterfaceBase):
     ret.pcmCruise = not ret.openpilotLongitudinalControl
     ret.autoResumeSng = ret.minEnableSpeed == -1
 
-    if ret.flags & VolkswagenFlags.MEB:
+    if ret.flags & (VolkswagenFlags.MEB | VolkswagenFlags.MQB_EVO):
       ret.startingState = True # OP long starting state is used: for very slow start the car can go into error (EPB car shutting down bug)
       ret.startAccel = 0.8
       ret.vEgoStarting = 0.5 # minimum ~0.5 m/s acc starting state is neccessary to not fault the car
