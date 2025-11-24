fc5facf98e41e3c442aae536b8241081e7c42505
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-11-24 20:35:08 +0100

    Update mebcan.py add import

--- opendbc/car/volkswagen/mebcan.py
+++ opendbc/car/volkswagen/mebcan.py
@@ -2,6 +2,7 @@ from opendbc.car.volkswagen.mebutils import map_speed_to_acc_tempolimit
 from opendbc.car.volkswagen.values import VolkswagenFlags
 from opendbc.car.volkswagen.speed_limit_manager import PSD_TYPE_CURV_SPEED
 from opendbc.car.common.conversions import Conversions as CV
+from opendbc.car.can_definitions import CanData
 
 ACCEL_INACTIVE = 3.01
 ACCEL_OVERRIDE = 0.00
