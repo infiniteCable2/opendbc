0fdad3698dd5cbe497a1ab1a47b751e3ebd784bc
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-11-29 19:17:30 +0100

    Update interface.py

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -87,7 +87,7 @@ class CarInterface(CarInterfaceBase):
       if 0x3DC in fingerprint[0]:  # Gatway_73
         ret.flags |= VolkswagenFlags.ALT_GEAR.value
        
-      if ret.networkLocation = NetworkLocation.fwdCamera:
+      if ret.networkLocation == NetworkLocation.fwdCamera and not (ret.flags & VolkswagenFlags.MQB_EVO):
         ret.flags |= VolkswagenFlags.DISABLE_RADAR.value
 
     elif ret.flags & VolkswagenFlags.MLB:
