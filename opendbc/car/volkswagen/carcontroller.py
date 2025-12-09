2a67b002bf9fe1dd89143ac819ad748f141b739a
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-12-09 22:32:01 +0100

    Update carcontroller.py

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -232,10 +232,12 @@ class CarController(CarControllerBase, IntelligentCruiseButtonManagementInterfac
           self.radar_disabled_warning_timer += 1
         if self.frame % self.CCP.AEB_CONTROL_STEP == 0:
           can_sends.append(make_tester_present_msg(0x700, self.CAN.pt, suppress_response=True)) # Tester Present
-          can_sends.append(self.CCS.create_aeb_control(self.packer_pt, self.CAN.pt, self.CP)) # AEB Control(1 Hz)
+          can_sends.append(self.CCS.create_aeb_control(self.packer_pt, self.CAN.pt, self.CP)) # AEB Control (1 Hz)
           can_sends.append(self.CCS.create_aeb_control(self.packer_pt, self.CAN.cam, self.CP)) # directed at the camera for Emergency Assist error mitigation
         if self.frame % self.CCP.AEB_HUD_STEP == 0:
           can_sends.append(self.CCS.create_aeb_hud(self.packer_pt, self.CAN.pt, self.radar_disabled_warning_timer < 600)) # AEB HUD (5 Hz)
+        if self.frame % 50 == 0:
+          can_sends.append(self.CCS.create_radar_unknown_01(self.packer_pt, self.CAN.pt, self.CP)) # pACC (2 Hz)
         if self.frame % 4 == 0: # not seen in MQBevo Gen 2 Audi RS3 2026
           can_sends.append(self.CCS.create_radar_distance(self.packer_pt, self.CAN.pt)) # Distance (25 Hz)
 
