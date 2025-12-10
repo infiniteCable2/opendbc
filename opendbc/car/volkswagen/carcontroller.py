0e86eef220b0d832f1e1bc02c37d5a0cc66c19a6
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-12-10 18:30:09 +0100

    Update carcontroller.py

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -231,16 +231,21 @@ class CarController(CarControllerBase, IntelligentCruiseButtonManagementInterfac
       if self.CP.flags & (VolkswagenFlags.MEB | VolkswagenFlags.MQB_EVO):
         if self.radar_disabled_warning_timer < 600: # display a hud warning for some seconds as hint for the user
           self.radar_disabled_warning_timer += 1
+          
         if self.frame % self.CCP.AEB_CONTROL_STEP == 0:
-          can_sends.append(make_tester_present_msg(0x700, self.CAN.pt, suppress_response=True)) # Tester Present
+          can_sends.append(make_tester_present_msg(0x700, self.CAN.pt, suppress_response=True)) # Tester Present to kepe the programming session
+          
           can_sends.append(self.CCS.create_aeb_control(self.packer_pt, self.CAN.pt, self.CP)) # AEB Control (1 Hz)
           can_sends.append(self.CCS.create_aeb_control(self.packer_pt, self.CAN.cam, self.CP)) # directed at the camera, prevent emergency assist error -> safety relevant
+          
         if self.frame % self.CCP.AEB_HUD_STEP == 0:
           can_sends.append(self.CCS.create_aeb_hud(self.packer_pt, self.CAN.pt, self.radar_disabled_warning_timer < 600)) # AEB HUD (5 Hz)
+          
         if self.frame % 50 == 0:
           can_sends.append(self.CCS.create_radar_pacc(self.packer_pt, self.CAN.pt, self.CP)) # pACC (2 Hz) prevent prdicative control error and keeps traffic sign detection working
-        #if self.frame % 4 == 0: # not seen in MQBevo Gen 2 Audi RS3 2026
-          #can_sends.append(self.CCS.create_radar_distance(self.packer_pt, self.CAN.pt)) # Distance (25 Hz)
+          
+        if self.frame % 4 == 0: # not seen in MQBevo Gen 2 Audi RS3 2026
+          can_sends.append(self.CCS.create_radar_distance(self.packer_pt, self.CAN.pt)) # Distance (25 Hz) works without (no erors in dash), but send it anyway for now
 
     # **** HUD Controls ***************************************************** #
 
