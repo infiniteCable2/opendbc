8fd3a6684f2694dd13ff50bcd84c8f09a94bfe4f
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-12-11 17:53:19 +0100

    Update volkswagen_meb.h

--- opendbc/safety/modes/volkswagen_meb.h
+++ opendbc/safety/modes/volkswagen_meb.h
@@ -48,12 +48,12 @@
   {MSG_KLR_01, 0, 8, .check_relay = false}, {MSG_KLR_01, 2, 8, .check_relay = true},       \
   {MSG_LDW_02, 0, 8, .check_relay = true}, {MSG_TA_01, 0, 8, .check_relay = true},         \
 
-#define VW_MEB_RADAR_TX_MSGS                                                                  \
-  {MSG_DIAG_RADAR, 0, 8, .check_relay = false},                                               \
-  {MSG_AWV_03, 0, 48, .check_relay = true},                                                   \
-  {MSG_MEB_Distance_01, 0, 64, .check_relay = true},                                          \
-  {MSG_MEB_AWV_01, 0, 8, .check_relay = true}, {MSG_MEB_AWV_01, 2, 8, .check_relay = false},  \
-  {MSG_MEB_PACC_01, 0, 8, .check_relay = true},                                               \
+#define VW_MEB_RADAR_TX_MSGS                                                                    \
+  {MSG_DIAG_RADAR, 0, 8, .check_relay = false},                                                 \
+  {MSG_MEB_Distance_01, 0, 64, .check_relay = true},                                            \
+  {MSG_AWV_03, 0, 48, .check_relay = true}, {MSG_AWV_03, 2, 48, .check_relay = false},          \
+  {MSG_MEB_AWV_01, 0, 8, .check_relay = true}, {MSG_MEB_AWV_01, 2, 8, .check_relay = false},    \
+  {MSG_MEB_PACC_01, 0, 8, .check_relay = true}, {MSG_MEB_PACC_01, 2, 8, .check_relay = false},  \
 
 static uint8_t volkswagen_crc8_lut_8h2f[256]; // Static lookup table for CRC8 poly 0x2F, aka 8H2F/AUTOSAR
 
