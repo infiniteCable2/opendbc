11c1172f8e0ef63e6d6d209517ac8a1b330602a9
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2026-04-13 17:04:18 +0200

    Update values.py lower

--- opendbc/car/volkswagen/values.py
+++ opendbc/car/volkswagen/values.py
@@ -110,7 +110,7 @@ class CarControllerParams:
       self.LDW_STEP                = 10    # LDW_02 message frequency 10Hz
       self.ACC_HUD_STEP            = 6     # MEB_ACC_01 message frequency 16Hz
       self.STEER_DRIVER_ALLOWANCE  = 60    # Driver torque 0.6 Nm, begin steering reduction from MAX
-      self.STEER_DRIVER_SLIGHT_PRESS = 20  # Driver torque 0.2 Nm for slight steering override detection
+      self.STEER_DRIVER_SLIGHT_PRESS = 15  # Driver torque 0.15 Nm for slight steering override detection
       self.STEER_DRIVER_MAX        = 300   # Driver torque 3.0 Nm, stop steering reduction at MIN
       self.STEERING_POWER_MAX      = 50    # HCA_03 maximum steering power, percentage
       self.STEERING_POWER_MIN      = 4     # HCA_03 minimum steering power, percentage
