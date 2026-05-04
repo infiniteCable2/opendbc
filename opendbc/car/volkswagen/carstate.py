78e7cdaa185c308546d29c240ad6cabc1e7e7d45
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2026-05-04 20:36:00 +0200

    Update carstate.py bsm probably on same sensor bus for gen 1 mqbevo too, not restrict to pt bus

--- opendbc/car/volkswagen/carstate.py
+++ opendbc/car/volkswagen/carstate.py
@@ -314,7 +314,7 @@ class CarState(CarStateBase, MadsCarState):
     # Consume blind-spot monitoring info/warning LED states, if available.
     # Infostufe: BSM LED on, Warnung: BSM LED flashing
     if self.CP.enableBsm:
-      bsm_bus = pt_cp if self.CP.flags & (VolkswagenFlags.MEB_GEN2 | VolkswagenFlags.MQB_EVO) else ext_cp
+      bsm_bus = pt_cp if (self.CP.flags & VolkswagenFlags.MEB_GEN2) else ext_cp
       blindspot_driver    = bool(bsm_bus.vl["MEB_Side_Assist_01"]["Blind_Spot_Info_Driver"]) or bool(bsm_bus.vl["MEB_Side_Assist_01"]["Blind_Spot_Warn_Driver"])
       blindspot_passenger = bool(bsm_bus.vl["MEB_Side_Assist_01"]["Blind_Spot_Info_Passenger"]) or bool(bsm_bus.vl["MEB_Side_Assist_01"]["Blind_Spot_Warn_Passenger"])
       car_is_lhd = True if not self.force_rhd_for_bsm else False # TODO
